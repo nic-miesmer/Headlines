5db9658a96433ae766766353f41c88970485b41c
nmiesmer <nic.miesmer@segmint.com>
2018-12-07 17:24:20 -0500

    some pseudocode and public folder

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const axios = require("axios");
-const cheerio = require("cheerio");
 const bodyparser = require("body-parser");
+//connection to mongodb
+const mongoose = require("mongoose");
+//promised based http library
+const axios = require("axios");
+//cheerio webscraping
+const cheerio = require("cheerio");
+
+// allows are database to use the schemas we defined in models directory
+const db = require("./models");
 
 const PORT = 3000;
 
@@ -10,6 +16,37 @@ const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true}));
 app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/headlines";
+
+mongoose.connect(MONGODB_URI,  { useNewUrlParser: true });
+
+//use this for now to check scrape like class activity
+const scrapeSite = "http://www.echojs.com/"
+
+
+//ENDPOINTS
+
+//scrape articles and save to DB
+
+//view all articles
+
+//view one article
+
+//leave a comment
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, function() {
